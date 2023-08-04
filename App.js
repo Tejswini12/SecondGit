4f14b0301bebd553476296a3fd87c39ac0ad8815
Tejswini12 <navgharetejswini03@gmail.com>
2023-08-04 14:11:00 +0530

    Done Changes with getElementByTagName

--- App.js
+++ App.js
@@ -20,7 +20,7 @@
 //headerTitle.innerHTML='<h3>Hello</h3>';
 //headerTitle.style.borderBottom='solid 3px #000';
 //header.style.borderBottom='solid 3px #000';
-var item=document.getElementsByClassName('list-group-item');
+//var item=document.getElementsByClassName('list-group-item');
 // console.log(item);
 // console.log(item[1]);
 // item[1].textContent='Hello 2';
@@ -29,7 +29,25 @@ var item=document.getElementsByClassName('list-group-item');
 // for(let i=0;i<item.length;i++){
 //     item[i].style.backgroundColor='#f4f4f4';
 // }
-item[2].style.backgroundColor='green';
-for(let i=0;i<item.length;i++){
-    item[i].style.fontWeight='bold';
+// item[2].style.backgroundColor='green';
+// for(let i=0;i<item.length;i++){
+//     item[i].style.fontWeight='bold';
+// }
+//bytagName
+// var li=document.getElementsByTagName('li');
+// console.log(li);
+// console.log(li[1]);
+// li[1].textContent='Hello 2';
+// li[1].style.fontWeight='bold';
+// li[1].style.backgroundColor='yellow';
+// for(let i=0;i<li.length;i++){
+//     li[i].style.backgroundColor='#f4f4f4';
+// }
+
+var item=document.getElementsByClassName('list-group-item-new');
+console.log(item);
+item[0].style.backgroundColor='pink';
+var newitem=document.getElementsByTagName('li');
+for(let i=0;i<newitem.length;i++){
+    newitem[i].style.fontSize='5px';
 }
