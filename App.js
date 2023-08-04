eb0982715e58b151e1f866cd3163f617840e6bf7
Tejswini12 <navgharetejswini03@gmail.com>
2023-08-04 19:17:12 +0530

    Changes done using QuerySelector

--- App.js
+++ App.js
@@ -44,10 +44,37 @@
 //     li[i].style.backgroundColor='#f4f4f4';
 // }
 
-var item=document.getElementsByClassName('list-group-item-new');
-console.log(item);
-item[0].style.backgroundColor='pink';
-var newitem=document.getElementsByTagName('li');
-for(let i=0;i<newitem.length;i++){
-    newitem[i].style.fontSize='5px';
+// var item=document.getElementsByClassName('list-group-item-new');
+// console.log(item);
+// item[0].style.backgroundColor='pink';
+// var newitem=document.getElementsByTagName('li');
+// for(let i=0;i<newitem.length;i++){
+//     newitem[i].style.fontSize='5px';
+// }
+//QuerySelector
+var header=document.querySelector('#main-header');
+header.style.borderBottom='solid 4px #ccc';
+var input=document.querySelector('input');
+input.value='Hello world';
+var submit=document.querySelector('input[type="submit"]');
+submit.value="SEND";
+var item=document.querySelector('.list-group-item');
+item.style.color='red';
+var lastItem=document.querySelector('.list-group-item:last-child');
+//lastItem.style.color='blue';
+
+var SecondlastItem=document.querySelector('.list-group-item:nth-child(2)');
+SecondlastItem.style.backgroundColor='green';
+//QurySelector ALL
+var titles=document.querySelectorAll('.title');
+console.log(titles);
+titles[0].textContent='Hello';
+titles[1].style.color='green';
+var odd=document.querySelectorAll('li:nth-child(odd)');
+for(var i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor='green';
+}
+var even=document.querySelectorAll('li:nth-child(even)');
+for(var i=0;i<even.length;i++){
+    even[i].style.backgroundColor='#ccc';
 }
